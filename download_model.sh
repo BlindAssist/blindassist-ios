025cc7bdc49232e95261cb9265a9c99795ee1d2e
Giovanni Terlingen <me@giovanniterlingen.com>
2018-08-22 11:14:56 +0200

    Update download url

--- download_model.sh
+++ download_model.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 OUTPUT_DIR="./BlindAssist/cityscapes.mlmodel"
-MODEL_URL="https://github.com/gi097/blindassist-scripts/releases/download/v1.0/cityscapes.mlmodel"
+MODEL_URL="https://github.com/BlindAssist/blindassist-scripts/releases/download/v1.0/cityscapes.mlmodel"
 
 echo "Starting mlmodel download..."
 
