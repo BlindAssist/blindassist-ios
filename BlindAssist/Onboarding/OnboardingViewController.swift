1e4832e7f7f9f0d63dfdf449133369d571119618
Khoa Pham <onmyway133@gmail.com>
2018-09-30 14:15:25 +0200

    Use texts to configure

--- BlindAssist/Onboarding/OnboardingViewController.swift
+++ BlindAssist/Onboarding/OnboardingViewController.swift
@@ -7,7 +7,43 @@
 //
 
 import UIKit
+import Anchors
 
 final class OnboardingViewController: UIViewController {
-  
+  private lazy var pagerView: PagerView = self.makePagerView()
+
+  // MARK: - Life cycle
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    view.addSubview(pagerView)
+    activate(
+      pagerView.anchor.edges
+    )
+  }
+
+  // MARK: - Make
+
+  private func makePagerView() -> PagerView {
+    let texts: [Text] = [
+      Text(title: "What", text: "An iOS app which will support the blind in traffic"),
+      Text(title: "Why", text: "To support blind people on the road"),
+      Text(title: "How", text: "Image segmentation to segment camera images in realtime")
+    ]
+
+    let pageViews: [PageView] = texts.map({ text in
+      let view = PageView()
+      view.titleLabel.text = text.title
+      view.textLabel.text = text.text
+      return view
+    })
+
+    return PagerView(views: pageViews)
+  }
+}
+
+private struct Text {
+  let title: String
+  let text: String
 }
--- BlindAssist/Onboarding/PageView.swift
+++ BlindAssist/Onboarding/PageView.swift
@@ -6,4 +6,58 @@
 //  Copyright © 2018 Giovanni Terlingen. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Anchors
+
+final class PageView: UIView {
+  private(set) lazy var backgroundImageView: UIImageView = {
+    let view = UIImageView()
+    view.contentMode = .scaleAspectFill
+    view.clipsToBounds = true
+
+    return view
+  }()
+
+  private(set) lazy var titleLabel: UILabel = {
+    let label = UILabel()
+    label.textAlignment = .center
+    label.textColor = UIColor.black
+    label.numberOfLines = 0
+    label.font = UIFont.preferredFont(forTextStyle: .headline)
+
+    return label
+  }()
+
+  private(set) lazy var textLabel: UILabel = {
+    let label = UILabel()
+    label.textAlignment = .center
+    label.textColor = UIColor.black
+    label.numberOfLines = 0
+    label.font = UIFont.preferredFont(forTextStyle: .title3)
+
+    return label
+  }()
+
+  // MARK: - Init
+
+  override init(frame: CGRect) {
+    super.init(frame: frame)
+
+    addSubview(backgroundImageView)
+    addSubview(titleLabel)
+    addSubview(textLabel)
+
+    activate(
+      backgroundImageView.anchor.edges,
+      titleLabel.anchor.centerY,
+      titleLabel.anchor.paddingHorizontally(20),
+      textLabel.anchor.top.equal.to(titleLabel.anchor.bottom).constant(20),
+      textLabel.anchor.paddingHorizontally(20)
+    )
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError()
+  }
+}
+
