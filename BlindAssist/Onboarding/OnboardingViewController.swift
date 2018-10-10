2ee62444e05f7506033d23018edcff0c0f9d440b
Khoa Pham <onmyway133@gmail.com>
2018-10-10 08:35:16 +0200

    Set background color

--- BlindAssist/Onboarding/OnboardingViewController.swift
+++ BlindAssist/Onboarding/OnboardingViewController.swift
@@ -17,6 +17,7 @@ final class OnboardingViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
 
+    view.backgroundColor = .white
     view.addSubview(pagerView)
     activate(
       pagerView.anchor.edges
