bbe5ff00042fe4e3aef6677c2d8e4548c96d1533
Khoa Pham <onmyway133@gmail.com>
2018-10-10 08:26:20 +0200

    Add MainController

---
+++ BlindAssist/Main/MainController.swift
@@ -0,0 +1,25 @@
+//
+//  MainController.swift
+//  BlindAssist
+//
+//  Created by khoa on 10.10.2018.
+//  Copyright © 2018 Giovanni Terlingen. All rights reserved.
+//
+
+import UIKit
+import Anchors
+
+final class MainViewController: UIViewController {
+
+  // MARK: - Init
+
+  required init() {
+    super.init(nibName: nil, bundle: nil)
+  }
+
+  required init?(coder aDecoder: NSCoder) {
+    fatalError()
+  }
+
+  // MARK: - Life cycle
+}
